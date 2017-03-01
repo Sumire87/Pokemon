32d796c8b0fe72949c83ce10576ab027e0e91a85
uzuma <uzuma@DESKTOP-3EOMDBN.lan>
2017-03-01 15:31:06 -0600

    Diseño terminado

---
+++ src/pokemon/Pokemon.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon;
+
+/**
+ *
+ * @author uzuma
+ */
+public class Pokemon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static Principal ventana = new Principal();
+    public static Formulario form = new Formulario();
+    
+    public static void main(String[] args) {
+        
+        ventana.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
